7b9dac43edf02d6aaf9051b050bfbc910387b761
unknown <kimhs1876>
2021-05-29 08:46:27 +0900

    movie_practice

--- webscarp/naver_movie2.py
+++ webscarp/naver_movie2.py
@@ -4,15 +4,24 @@ import pandas as pd
 
 class NaverMovies(object):
     url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
-    dirve = path = 'C:/Program Files/Google/Chrome/chromedriver'
-    class_name = ''
+    dirve_path = 'C:/Program Files/Google/Chrome/chromedriver'
+    class_name = []
     title_list = []
     dict = {}
     df = None
 
     def scrap(self):
-        pass
+        driver = webdriver.Chrome(self.driver_path)
+        driver.get(self.url)
+        soup = BeautifulSoup(driver, page_source, 'html.parser')
+        all_div = soup.find_all('div', {"class" : self.class_name}) # 구글검색! #
+        self.title_list = [ i.find('a').text for i in all_div]
+        driver.close()
+
+
+
 
 if __name__ == '__main__':
     naver = NaverMovies()
+
     
